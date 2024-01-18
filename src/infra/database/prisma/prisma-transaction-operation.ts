bcd0bb72a75146710b7f10f66172dfc1819959bf
joaov-rizzardo <jvitor2000@hotmail.com>
2024-01-18 10:38:06 -0300

    Feat: Abstracting repositories transactions

--- src/infra/database/prisma/prisma-transaction-operation.ts
+++ src/infra/database/prisma/prisma-transaction-operation.ts
@@ -8,11 +8,14 @@ export class PrismaTransactionOperation implements RunTransactionOperation {
   constructor(private readonly prisma: PrismaProvider) {}
 
   async execute<T>(callback: () => T | Promise<T>): Promise<T> {
-    return this.prisma.transactionContext.$transaction(async (prisma) => {
-      this.prisma.startTransactionContext(prisma as PrismaClient);
-      const response = await callback();
-      this.prisma.resetTransactionContext();
-      return response;
-    });
+    return this.prisma.transactionContext
+      .$transaction(async (prisma) => {
+        this.prisma.startTransactionContext(prisma as PrismaClient);
+        const response = await callback();
+        return response;
+      })
+      .finally(() => {
+        this.prisma.resetTransactionContext();
+      });
   }
 }
